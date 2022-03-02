6a8e22a84e03423fbff8737ce3d3d36bf36af5a3
Xiasong Chen <296724771@qq.com>
2022-03-02 15:34:26 +0000

    init

---
+++ src/test/java/Prac/TestPWD.java
@@ -0,0 +1,14 @@
+package Prac;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestPWD {
+    @Test
+    public void text()
+    {
+        PasswordValidator pv=new PasswordValidator();
+        Assert.assertEquals(true,pv.isValid("abc123"));
+    }
+}
