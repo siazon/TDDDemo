d155b5cea2a61ddb80070ec80eed980f875ac4a4
Xiasong Chen <296724771@qq.com>
2022-03-02 20:13:36 +0000

    add test copy code

--- src/main/java/PracSrc/PasswordValidator.java
+++ src/main/java/PracSrc/PasswordValidator.java
@@ -3,9 +3,19 @@ package PracSrc;
 public class PasswordValidator {
     public  boolean isValid(String pwd)
     {
-        if(pwd.length()>3)
+        if(pwd.length()>6)
             return true;
         else
         return false;
+
+    }
+    // test copy code
+    public  boolean isValid1(String pwd)
+    {
+        if(pwd.length()>6)
+            return true;
+        else
+            return false;
+
     }
 }
