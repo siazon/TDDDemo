b2c3e6c6ea05816315278e0072d0d3ef0c42916b
Xiasong Chen <296724771@qq.com>
2022-03-06 21:16:42 +0000

    pipeline

--- src/main/java/PracSrc/PasswordValidator.java
+++ src/main/java/PracSrc/PasswordValidator.java
@@ -4,22 +4,11 @@ public class PasswordValidator {
     public static boolean isValid(String pwd)
     {
         //test bug
-        if(pwd.length()>3) {
+        if(pwd.length()>5) {
             return true;
         }
         else
         return false;
-
-    }
-    // test copy code
-    public  boolean isValid1(String pwd)
-    {
-
-
-        if(pwd.length()>6)
-            return true;
-        else
-            return false;
 
     }
 }
---
+++ src/test/java/PracSrc/PasswordValidatorTest.java
@@ -0,0 +1,16 @@
+package PracSrc;
+
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @Test
+    void isValid() {
+        Assert.assertEquals(true,PasswordValidator.isValid("abc123"));
+        Assert.assertEquals(false,PasswordValidator.isValid("abc1"));
+    }
+
+}
